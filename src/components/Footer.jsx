68f8f5012898e535108bd6644d8a03e19bbfbbd1
Habtamu <habtamukasahun47@gmail.com>
2025-10-23 17:51:12 +0300

    Add Footer component with copyright

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-10 text-center">
+      &copy; 2025 AI Content Creator Hub
+    </footer>
+  );
+}
+
+export default Footer;
